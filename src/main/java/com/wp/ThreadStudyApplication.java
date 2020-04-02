c90f8c4e6753c595d1f3a8a3d1d943bd310d478f
wangpeng116 <wangpeng116@longfor.com>
2020-04-02 18:32:22 +0800

    主类

---
+++ src/main/java/com/wp/ThreadStudyApplication.java
@@ -0,0 +1,17 @@
+package com.wp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Classname ThreadStudyApplication
+ * @Description 主类
+ * @Date 2020/4/2 18:31
+ * @Created by wangpeng116
+ */
+@SpringBootApplication
+public class ThreadStudyApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ThreadStudyApplication.class, args);
+    }
+}
