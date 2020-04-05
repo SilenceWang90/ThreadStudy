ad30208359f30503e1e0176d0bdf1d564b2fdd4e
wangpeng116 <wangpeng116@longfor.com>
2020-04-05 10:23:39 +0800

    '错误创建线程的方式'

--- src/main/java/com/wp/threadcoreknowledge/wrongways/DemoTimmerTask.java
+++ src/main/java/com/wp/threadcoreknowledge/wrongways/DemoTimmerTask.java
@@ -1,10 +1,23 @@
 package com.wp.threadcoreknowledge.wrongways;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 /**
  * @Classname DemoTimmerTask
- * @Description TODO
+ * @Description 定时器创建线程
  * @Date 2020/4/5 10:16
  * @Created by wangpeng116
  */
 public class DemoTimmerTask {
+    public static void main(String[] args) {
+        Timer timer = new Timer();
+        //每隔一秒钟打印一下线程名称
+        timer.scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                System.out.println(Thread.currentThread().getName());
+            }
+        }, 1000, 1000);
+    }
 }
--- src/main/java/com/wp/threadcoreknowledge/wrongways/ThreadPool.java
+++ src/main/java/com/wp/threadcoreknowledge/wrongways/ThreadPool.java
@@ -1,10 +1,32 @@
 package com.wp.threadcoreknowledge.wrongways;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 /**
  * @Classname ThreadPool
- * @Description TODO
+ * @Description 线程池创建线程
+ * 通过ThreadFactory创建线程，该线程工厂本质是通过传入Runnable接口
  * @Date 2020/4/5 10:07
  * @Created by wangpeng116
  */
 public class ThreadPool {
+    public static void main(String[] args) {
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        for (int i = 0; i < 1000; i++) {
+            executorService.submit(new Task());
+        }
+    }
+}
+
+class Task implements Runnable {
+    @Override
+    public void run() {
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println(Thread.currentThread().getName());
+    }
 }
