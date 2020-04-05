71860d26c6c20a7fd482649460c817e2d1ac5210
wangpeng116 <wangpeng116@longfor.com>
2020-04-05 10:22:13 +0800

    '错误创建线程的方式'

---
+++ src/main/java/com/wp/threadcoreknowledge/wrongways/DemoTimmerTask.java
@@ -0,0 +1,10 @@
+package com.wp.threadcoreknowledge.wrongways;
+
+/**
+ * @Classname DemoTimmerTask
+ * @Description TODO
+ * @Date 2020/4/5 10:16
+ * @Created by wangpeng116
+ */
+public class DemoTimmerTask {
+}
---
+++ src/main/java/com/wp/threadcoreknowledge/wrongways/ThreadPool.java
@@ -0,0 +1,10 @@
+package com.wp.threadcoreknowledge.wrongways;
+
+/**
+ * @Classname ThreadPool
+ * @Description TODO
+ * @Date 2020/4/5 10:07
+ * @Created by wangpeng116
+ */
+public class ThreadPool {
+}
