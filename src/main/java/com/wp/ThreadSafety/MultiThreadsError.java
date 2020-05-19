38350fe9365cae634c1dcaef7279d1bbea146edd
wangpeng116 <wangpeng116@longfor.com>
2020-05-19 15:04:15 +0800

    线程安全

--- src/main/java/com/wp/ThreadSafety/MultiThreadsError.java
+++ src/main/java/com/wp/ThreadSafety/MultiThreadsError.java
@@ -1,5 +1,9 @@
 package com.wp.ThreadSafety;
 
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.atomic.AtomicInteger;
+
 /**
  * @Classname MultiThreadsError
  * @Description 运行结果出错
@@ -9,8 +13,13 @@ package com.wp.ThreadSafety;
  */
 public class MultiThreadsError implements Runnable {
     int index = 0;
-
+    final boolean[] marked = new boolean[10000000];
+    static AtomicInteger realIndex = new AtomicInteger();
+    static AtomicInteger wrongIndex = new AtomicInteger();
     static MultiThreadsError instance = new MultiThreadsError();
+    //参数2代表需要等待几个线程
+    static volatile CyclicBarrier cyclicBarrier1 = new CyclicBarrier(2);
+    static volatile CyclicBarrier cyclicBarrier2 = new CyclicBarrier(2);
 
     public static void main(String[] args) throws InterruptedException {
         Thread thread1 = new Thread(instance, "thread1");
@@ -21,13 +30,23 @@ public class MultiThreadsError implements Runnable {
         thread1.join();
         //主线程等线程2执行完
         thread2.join();
-        System.out.println(instance.index);
+        System.out.println("表面上结果是：" + instance.index);
+        System.out.println("真正运行的参数" + realIndex.get());
+        System.out.println("错误次数" + wrongIndex.get());
     }
 
     @Override
     public void run() {
         for (int i = 0; i < 10000; i++) {
             index++;
+            realIndex.incrementAndGet();
+            synchronized (instance) {
+                if (marked[index] && marked[index - 1]) {
+                    System.out.println("发生错误" + index);
+                    wrongIndex.incrementAndGet();
+                }
+                marked[index] = true;
+            }
         }
     }
 }
