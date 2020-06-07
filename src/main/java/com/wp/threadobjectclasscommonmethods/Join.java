49eb4336da1f5fa35d8dda2fb06b7fc491a8ff9d
wangpeng116 <wangpeng116@longfor.com>
2020-06-07 17:39:36 +0800

    Join普通用法

---
+++ src/main/java/com/wp/threadobjectclasscommonmethods/Join.java
@@ -0,0 +1,37 @@
+package com.wp.threadobjectclasscommonmethods;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @Classname Join
+ * @Description 演示Join
+ * @Date 2020/6/7 17:30
+ * @Created by wangpeng116
+ */
+@Slf4j
+public class Join {
+    public static void main(String[] args) throws InterruptedException {
+        Thread thread1 = new Thread(() -> {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println(Thread.currentThread().getName() + "执行完毕");
+        });
+        Thread thread2 = new Thread(() -> {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println(Thread.currentThread().getName() + "执行完毕");
+        });
+        thread1.start();
+        thread2.start();
+        System.out.println("开始等待子线程运行完毕");
+        thread1.join();
+        thread2.join();
+        System.out.println("所有子线程执行完毕");
+    }
+}
