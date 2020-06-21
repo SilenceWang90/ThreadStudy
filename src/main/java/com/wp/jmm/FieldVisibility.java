26b2233b786b37c2297d0d27bb3a2db1cb4ad716
wangpeng116 <wangpeng116@longfor.com>
2020-06-21 19:29:43 +0800

    JMM：可见性

---
+++ src/main/java/com/wp/jmm/FieldVisibility.java
@@ -0,0 +1,44 @@
+package com.wp.jmm;
+
+/**
+ * @Classname FieldVisibility
+ * @Description 可见性
+ * @Date 2020/6/21 17:50
+ * @Created by wangpeng116
+ */
+public class FieldVisibility {
+    int a = 1;
+    int b = 2;
+
+    private void change() {
+        a = 3;
+        b = a;
+    }
+
+    private void print() {
+        System.out.println("b=" + b + ";a=" + a);
+    }
+
+    public static void main(String[] args) {
+        while (true) {
+            FieldVisibility test = new FieldVisibility();
+            new Thread(() -> {
+                try {
+                    Thread.sleep(1);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                test.change();
+            }).start();
+
+            new Thread(() -> {
+                try {
+                    Thread.sleep(1);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                test.print();
+            }).start();
+        }
+    }
+}
